0232785e49c9492b6a24a1cc47ff5f0f5fb71bb2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-26 10:29:37 +0530

    adding libraries and constants

--- rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
+++ rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.rectcreatethenmoveview
 /**
  * Created by anweshmishra on 26/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val rects : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
