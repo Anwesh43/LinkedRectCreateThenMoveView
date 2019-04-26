49dc211eba9e56b6a581292bea676c28f3ab717a
Anwesh43 <anweshthecool0@gmail.com>
2019-04-26 11:10:21 +0530

    adding RectCreateThenMove class

--- rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
+++ rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
@@ -171,4 +171,27 @@ class RectCreateThenMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RectCreateThenMove(var i : Int) {
+
+        private var curr : RCTMNode = RCTMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
