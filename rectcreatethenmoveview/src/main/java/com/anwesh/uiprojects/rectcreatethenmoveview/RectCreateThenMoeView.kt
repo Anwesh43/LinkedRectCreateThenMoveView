47715eda17081f8e66f0351a4713bed9ad396c93
Anwesh43 <anweshthecool0@gmail.com>
2019-04-26 11:00:44 +0530

    adding RectCreateThenMoveView class

--- rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
+++ rectcreatethenmoveview/src/main/java/com/anwesh/uiprojects/rectcreatethenmoveview/RectCreateThenMoeView.kt
@@ -62,5 +62,22 @@ fun Canvas.drawRCTMNode(i : Int, scale : Float, paint : Paint) {
         restore()
     }
     restore()
+}
 
+class RectCreateThenMoveView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
 }
